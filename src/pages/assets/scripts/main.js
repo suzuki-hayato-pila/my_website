b541146e8d07e12afaf7a6c4999b8ed9d9dfba3b
suzuki-hayato-pila <suzuki.hayato.jellyware@gmail.com>
2025-11-24 14:37:00 +0900

    SVGアニメーション終了

--- src/pages/assets/scripts/main.js
+++ src/pages/assets/scripts/main.js
@@ -1,5 +1,7 @@
+import { heroAnimation } from "./modules/heroAnimation";
 import { nav } from "./modules/nav";
 import { scrollAnimation } from "./modules/scrollAnimation";
 
 nav();
 scrollAnimation();
+heroAnimation();
---
+++ src/pages/assets/scripts/modules/heroAnimation.js
@@ -0,0 +1,14 @@
+import anime from "animejs/lib/anime.es.js";
+
+export const heroAnimation = () => {
+    // ヒーローアニメーション
+    anime ({
+        targets: "#js-hero-animation path",
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function(el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: true
+    });
+};
